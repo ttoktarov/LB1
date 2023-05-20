2d37f9d1a7b735a954f1e7bcf8f944fb26d9e2dd
ttoktarov <ttoktarov@gmail.com>
2023-05-20 14:32:16 +0300

    port3000

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+
+    console.log(`Сервер запущен. Порт сервера: ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
